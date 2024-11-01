c7acd45000af6eabdefe45be6584b506d46e7baa
lgrin-byte <lgrin02@naver.com>
2024-11-01 17:46:01 +0900

    fix: 버튼 클릭시 outside함수 수정

--- app/hooks/useClickOutside.ts
+++ app/hooks/useClickOutside.ts
@@ -5,24 +5,20 @@ function useClickOutside<T extends HTMLElement>(
   onClickOutside: () => void
 ) {
   useEffect(() => {
-    /**
-     * Invoke Function onClick outside of element
-     */
-    function handleClickOutside(event: MouseEvent) {
-      // 이벤트가 상위로 전파되지 않도록 수정
+    const handleClickOutside = (event: MouseEvent) => {
+      // 클릭한 요소가 ref.current의 자식이 아닌 경우
       if (ref.current && !ref.current.contains(event.target as Node)) {
-        onClickOutside();
-        event.stopPropagation(); // 상위 모달로의 이벤트 전파를 막음
+        onClickOutside(); // 외부 클릭 처리
       }
-    }
-
-    // Bind
-    document.addEventListener("click", handleClickOutside);
-    return () => {
-      // dispose
-      document.removeEventListener("click", handleClickOutside);
     };
-  }, [ref, onClickOutside]);
+
+    // 이벤트 리스너 등록
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // 컴포넌트가 언마운트될 때 이벤트 리스너 제거
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref, onClickOutside]); // ref와 onClickOutside에 의존성 추가
 }
 
 export default useClickOutside;
