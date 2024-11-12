065b4f4f512e855725c1590f24d61a8ac2e86147
lgrin-byte <lgrin02@naver.com>
2024-11-12 09:17:15 +0900

    feat: useClickOutside 훅 수정

--- app/hooks/useClickOutside.ts
+++ app/hooks/useClickOutside.ts
@@ -1,7 +1,7 @@
 import { useEffect, RefObject } from "react";
 
 /**
- * 이벤트 이외 영역 클릭 시 이벤트 닫아주는 공통 hook
+ * 클릭 시 이벤트를 닫아주는 공통 hook
  * @param {RefObject<HTMLElement | HTMLElement[]>} ref - Element ref
  * @param {Function} handler - 이벤트 핸들러
  * @param {boolean} isActive - 활성화 유무
@@ -11,53 +11,36 @@ import { useEffect, RefObject } from "react";
 function useClickOutside(
   ref: RefObject<HTMLElement | HTMLElement[]>,
   handler: (event: MouseEvent) => void,
-  isActive: boolean = true,
-  targetIndex: number = 0,
-  ignoreSiblings: boolean = false
+  isActive = true,
+  targetIndex = 0,
+  ignoreSiblings = false
 ) {
   useEffect(() => {
-    if (!isActive) {
-      return;
-    }
+    if (!isActive) return;
 
     const listener = (event: MouseEvent) => {
-      if (!ref.current) {
-        return;
-      }
+      const target = event.target as HTMLElement;
 
-      if (Array.isArray(ref.current)) {
-        if (
-          ref.current[targetIndex] &&
-          ref.current[targetIndex].contains(event.target as Node)
-        ) {
+      // ref가 비어 있거나, ref 내부를 클릭한 경우 무시
+      const elements = Array.isArray(ref.current) ? ref.current : [ref.current];
+      const isClickInside = elements.some((element) => element?.contains(target));
+      if (isClickInside) return;
+
+      // ignoreSiblings 옵션이 true인 경우 형제 요소 클릭 시 닫음
+      if (ignoreSiblings) {
+        const parentNode = elements[targetIndex]?.parentNode;
+        if (parentNode && Array.from(parentNode.children).some((sibling) => sibling.contains(target))) {
           return;
         }
-      } else {
-        if (ref.current.contains(event.target as Node)) {
-          return;
-        }
-
-        // ignoreSiblings 옵션이 true인 경우 형제 요소 클릭 시 ref를 닫음
-        if (ignoreSiblings && ref.current.parentNode) {
-          const siblings = Array.from(ref.current.parentNode.children);
-          if (
-            siblings.some((sibling) => sibling.contains(event.target as Node))
-          ) {
-            return;
-          }
-        }
       }
 
-      if (handler) {
-        handler(event);
-      }
+      // 핸들러 실행 (버튼이든 어떤 요소든 외부 클릭이면)
+      handler(event);
     };
 
     document.addEventListener("click", listener);
-
-    return () => {
-      document.removeEventListener("click", listener);
-    };
+    return () => document.removeEventListener("click", listener);
   }, [ref, handler, isActive, targetIndex, ignoreSiblings]);
 }
+
 export default useClickOutside;
